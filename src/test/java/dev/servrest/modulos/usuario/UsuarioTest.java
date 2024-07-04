048e82b5ba0be59674d4a87347b5320421575038
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-07-04 19:58:30 -0300

    Adicionando novos testes para a classe de Usuario

--- src/test/java/dev/servrest/modulos/usuario/UsuarioTest.java
+++ src/test/java/dev/servrest/modulos/usuario/UsuarioTest.java
@@ -2,12 +2,11 @@ package dev.servrest.modulos.usuario;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UsuarioTest {
 
@@ -100,6 +99,54 @@ public class UsuarioTest {
                 .assertThat()
                 .body("password", equalTo("password não pode ficar em branco"))
                 .statusCode(400)
+                .extract().response();
+
+        System.out.println(response.asString());
+    }
+
+    @Test
+    @DisplayName("Validar Login com email em branco")
+    public void testRealizarLoginComEmailEmBranco() {
+
+        baseURI = "http://localhost";
+        port = 3000;
+
+        this.response = given().
+                contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"email\": \"\",\n" +
+                        "  \"password\": \"teste\"\n" +
+                        "}")
+                .when()
+                .post("/login")
+                .then()
+                .assertThat()
+                .body("email", equalTo("email não pode ficar em branco"))
+                .statusCode(400)
+                .extract().response();
+
+        System.out.println(response.asString());
+    }
+
+    @Test
+    @DisplayName("Validar Login com senha incorreta")
+    public void testRealizarLoginComSenhaIncorreta() {
+
+        baseURI = "http://localhost";
+        port = 3000;
+
+        this.response = given().
+                contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"email\": \"fulano@qa.com\",\n" +
+                        "  \"password\": \"teste123\"\n" +
+                        "}")
+                .when()
+                .post("/login")
+                .then()
+                .assertThat()
+                .body("message", equalTo("Email e/ou senha inválidos"))
+                .statusCode(401)
                 .extract().response();
 
         System.out.println(response.asString());
