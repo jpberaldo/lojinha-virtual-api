7522f4f3d3ae0d528a8f5bf40a519fc600550fd8
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-07-08 17:33:21 -0300

    Implementação de testes que validam a classe de Produtos

--- src/test/java/dev/servrest/modulos/produto/ProdutoTest.java
+++ src/test/java/dev/servrest/modulos/produto/ProdutoTest.java
@@ -1,4 +1,73 @@
 package dev.servrest.modulos.produto;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class ProdutoTest {
+
+    Response response;
+
+    @Test
+    @DisplayName("Exibir lista de produtos cadastrados")
+    public void testExibirListaDeUsuariosCadastrados() {
+
+        baseURI = "http://localhost";
+        port = 3000;
+
+        this.response = given().
+                when().
+                get("/produtos")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        System.out.println(response.asString());
+
+    }
+
+    @Test
+    @DisplayName("Cadastrar novo produto válido")
+    public void testCadastrarNovoProdutoValido() {
+
+        baseURI = "http://localhost";
+        port = 3000;
+
+        String token = given().
+                contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"email\": \"fulano@qa.com\",\n" +
+                        "  \"password\": \"teste\"\n" +
+                        "}")
+                .when()
+                .post("/login")
+                .then()
+                .extract().path("authorization");
+
+        System.out.println(token);
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("authorization", token)
+                .body("{\n" +
+                        "  \"nome\": \"Logitech GPRO Pink\",\n" +
+                        "  \"preco\": 600,\n" +
+                        "  \"descricao\": \"Mouse\",\n" +
+                        "  \"quantidade\": 80\n" +
+                        "}")
+                .when()
+                .post("/produtos")
+                .then()
+                .assertThat()
+                .body("message", equalTo("Cadastro realizado com sucesso"))
+                .extract().response();
+
+        System.out.println(response);
+
+    }
+
 }
