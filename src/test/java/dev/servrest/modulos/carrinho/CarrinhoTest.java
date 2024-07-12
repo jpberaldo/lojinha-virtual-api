7ea6d8189abe3ebb3e87f34316e1321322dd639a
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-07-12 18:45:08 -0300

    Implementação de teste na classe de Carrinho

--- src/test/java/dev/servrest/modulos/carrinho/CarrinhoTest.java
+++ src/test/java/dev/servrest/modulos/carrinho/CarrinhoTest.java
@@ -1,4 +1,31 @@
 package dev.servrest.modulos.carrinho;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class CarrinhoTest {
+
+    Response response;
+
+    @Test
+    @DisplayName("Exibir lista de produtos cadastrados")
+    public void testExibirListaDeUsuariosCadastrados() {
+
+        baseURI = "http://localhost";
+        port = 3000;
+
+        this.response = given().
+                when().
+                get("/carrinhos")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        System.out.println(response.asString());
+
+    }
+
 }
