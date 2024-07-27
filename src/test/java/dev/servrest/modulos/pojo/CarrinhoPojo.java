b3b0efcd5938a5ac2ab1d11fc9822712c8a9d9c5
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-07-27 18:50:44 -0300

    Organização e geração de parâmetros do CarrinhoPojo

--- src/test/java/dev/servrest/modulos/pojo/CarrinhoPojo.java
+++ src/test/java/dev/servrest/modulos/pojo/CarrinhoPojo.java
@@ -1,4 +1,23 @@
 package dev.servrest.modulos.pojo;
 
 public class CarrinhoPojo {
+
+    private String idProduto;
+    private int quantidade;
+
+    public String getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(String idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
