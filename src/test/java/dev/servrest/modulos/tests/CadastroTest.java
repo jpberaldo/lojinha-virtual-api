712e3bb4f7225d132d34e77757d6783d194fe2fa
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-07-29 17:49:11 -0300

    Implementação de novos testes de Cadastro

--- src/test/java/dev/servrest/modulos/tests/CadastroTest.java
+++ src/test/java/dev/servrest/modulos/tests/CadastroTest.java
@@ -1,16 +1,17 @@
 package dev.servrest.modulos.tests;
 
 import dev.servrest.modulos.data.CadastroData;
+import dev.servrest.modulos.data.UsuarioData;
 import dev.servrest.modulos.utils.Service;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CadastroTest {
 
     Response response;
@@ -21,18 +22,67 @@ public class CadastroTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("Validar que não permite fazer cadastro com um usuário já cadastrado no sistema")
     public void testValidarQueCadastrarUsuarioComDadosJaUtilizados() {
 
         this.response = given().
                 contentType(ContentType.JSON)
-                .body(CadastroData.informarDadosDeCadastro("Fulano da Silva","beltrano@qa.com.br","teste","true"))
+                .body(CadastroData.informarDadosDeCadastro("Fulano da Silva", "beltrano@qa.com.br", "teste", "true"))
                 .when()
                 .post("/usuarios")
                 .then()
                 .assertThat()
                 .statusCode(400)
                 .body("message", equalTo("Este email já está sendo usado"))
+                .extract().response();
+
+        System.out.println(response.asString());
+
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Cadastrar novo usuario com dados validos")
+    public void testCadastrarNovoUsuarioComSucesso() {
+
+
+        response = given()
+                .contentType(ContentType.JSON)
+                .body(CadastroData.informarDadosDeCadastro("Joao", "teste@emailnovo.com", "teste", "true"))
+                .when()
+                .post("/usuarios")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .body("message", equalTo("Cadastro realizado com sucesso"))
+                .extract().response();
+
+        System.out.println(response.asString());
+
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Excluir um usuário existente")
+    public void testExcluirUmUsuarioExistente() {
+
+        Response usuariosResponse = given()
+                .when()
+                .get("/usuarios")
+                .then()
+                .extract().response();
+
+        String idUsuario = usuariosResponse.jsonPath().getString("usuarios[4]._id");
+
+        response = given()
+                .pathParam("_id", idUsuario)
+                .when()
+                .delete("/usuarios/{_id}")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("message", equalTo("Registro excluído com sucesso"))
                 .extract().response();
 
         System.out.println(response.asString());
