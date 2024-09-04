54b73f245ed9e512ea5b0d5cdab073c94d321498
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-09-03 22:48:41 -0300

    Melhoria nas classes que se referem a classe Carrinho

--- src/test/java/dev/servrest/modulos/data/CarrinhoData.java
+++ src/test/java/dev/servrest/modulos/data/CarrinhoData.java
@@ -1,4 +1,15 @@
 package dev.servrest.modulos.data;
 
+import dev.servrest.modulos.pojo.CarrinhoPojo;
+import dev.servrest.modulos.pojo.ProdutoPojo;
+
+import java.util.List;
+
 public class CarrinhoData {
+
+    public static CarrinhoPojo informarDadosParaCarrinho(List<ProdutoPojo> produtos) {
+        CarrinhoPojo carrinho = new CarrinhoPojo();
+        carrinho.setProdutos(produtos);
+        return carrinho;
+    }
 }
--- src/test/java/dev/servrest/modulos/pojo/CarrinhoPojo.java
+++ src/test/java/dev/servrest/modulos/pojo/CarrinhoPojo.java
@@ -1,23 +1,16 @@
 package dev.servrest.modulos.pojo;
 
+import java.util.List;
+
 public class CarrinhoPojo {
 
-    private String idProduto;
-    private int quantidade;
+    private List<ProdutoPojo> produtos;
 
-    public String getIdProduto() {
-        return idProduto;
+    public List<ProdutoPojo> getProdutos() {
+        return produtos;
     }
 
-    public void setIdProduto(String idProduto) {
-        this.idProduto = idProduto;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
+    public void setProdutos(List<ProdutoPojo> produtos) {
+        this.produtos = produtos;
     }
 }
