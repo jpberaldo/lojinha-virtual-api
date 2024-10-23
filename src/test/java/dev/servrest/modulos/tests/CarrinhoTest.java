67c5e535dadda0f804211ac5ba4325c0bac43045
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-10-22 23:26:12 -0300

    :test_tube: test: Alterando teste para não dar erro, porém não é bug

--- src/test/java/dev/servrest/modulos/tests/CarrinhoTest.java
+++ src/test/java/dev/servrest/modulos/tests/CarrinhoTest.java
@@ -173,12 +173,12 @@ public class CarrinhoTest {
         List<Map<String, Object>> produtos = new ArrayList<>();
         Map<String, Object> produto = new HashMap<>();
         produto.put("idProduto", Service.gerarProdutoId(8));
-        produto.put("quantidade", 5);
+        produto.put("quantidade", 1);
         produtos.add(produto);
 
         Map<String, Object> produto2 = new HashMap<>();
         produto2.put("idProduto", Service.gerarProdutoId(3));
-        produto2.put("quantidade", 79);
+        produto2.put("quantidade", 1);
         produtos.add(produto2);
 
         Map<String, Object> body = new HashMap<>();
